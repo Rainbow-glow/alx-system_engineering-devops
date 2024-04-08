8d741e68a66b5047e123a6d01de3800a69b415e6
Rainbow-glow <emmanuellanweze97@gmail.com>
2024-04-08 09:08:44 -0700

    fixing errors

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,12 @@
-# Installs flask -v 2.1.0
+# Installs pip3
+package{ 'python3-pip':
+  ensure => installed,
+} 
 
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
+
+# install flask -v 2.1.0 usin pip3
+
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
