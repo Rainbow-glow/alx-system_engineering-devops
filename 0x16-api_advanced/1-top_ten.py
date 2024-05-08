f6afff0d288987725e4d4f46e273a8322c618e51
Your Name <you@example.com>
2024-05-08 13:49:22 -0700

    fixing

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,31 @@
 #!/usr/bin/python3
-"""Function to print the title of 10 hot posts on a given Reddit subreddit."""
+"""
+A module to retrieve and print the titles of the first 10 hot posts for a given
+subreddit using the Reddit API.
+"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "bhalut_")
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
+    """
+    Prints the titles of the first 10 hot posts listed for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {'User-Agent': 'Custom User Agent'}
+    params = {'limit': 10}
+
+    response = requests.get(url, headers=headers, params=params)
+    if response.status_code == 200:
+        data = response.json().get('data', {})
+        children = data.get('children', [])
+        if children:
+            for i, child in enumerate(children, 1):
+                if not child['data']['stickied']:  # Skip stickied posts
+                    print(f"{i}. {child['data']['title']}")
+        else:
+            print("No hot posts found for this subreddit.")
+    else:
         print("None")
-        return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
