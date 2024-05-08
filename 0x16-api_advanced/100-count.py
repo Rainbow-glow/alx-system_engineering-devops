996f7c45280df31da652b19b8b1a271faf6a3815
Your Name <you@example.com>
2024-05-08 08:07:49 -0700

    api advanced tasks fixed

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Function to count words in all hot posts of a given Reddit subreddit."""
+import re
 import requests
+import sys
 
 
 def count_words(subreddit, word_list, instances={}, after="", count=0):
