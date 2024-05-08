e4987bcc7f9d1c02b0db796749541e2026d5a783
Your Name <you@example.com>
2024-05-08 08:22:36 -0700

    api advanced tasks fixed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,11 +8,11 @@ def number_of_subscribers(subreddit):
     """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {
-            "User-Agent": "Google chrome:0x16.api.advanced:v1.0.0 (by /u/ranebo)"
+            "User-Agent": "Google Chrome:0x16.api.advanced:v1.0.0 (by /u/ranebo)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
         return 0
     else:
-        results = response.json().get("data")
+        results = response.json()
         return results.get("data").get("subscribers")
