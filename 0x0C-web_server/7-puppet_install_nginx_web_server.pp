a7dde10466dbac412f580d2fcd67233d0b7461fb
Your Name <you@example.com>
2024-05-22 13:52:49 -0700

    redoing my tasks

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,18 +1,19 @@
-# using Puppet| Install Nginx server, setup and configuration
+#!/usr/bin/env bash
+# Installs and configure an Nginx server using Puppet instead of Bash.
 
 package { 'nginx':
-  ensure => 'installed'
+  ensure => installed,
 }
 
-file { '/var/www/html/index.html':
-  content => 'Hello World',
-}
-
-file_line { 'redirection-301':
+file_line { 'Add redirection, 301':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect-me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
 }
 
 service { 'nginx':
